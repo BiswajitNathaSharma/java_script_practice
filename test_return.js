4b91b0c954ba31a6880127d1b99106cee97429ff
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-05-15 09:37:57 +0530

    examining use of return statement
    in this code, I Im checking that any statement after return statement is runing or not, and I get the answer is no.

---
+++ test_return.js
@@ -0,0 +1,9 @@
+// creating a function named sum and i want to see that, any statement after return will execute or not, i find that it dosen't execute 
+function sum(a,b){
+    return a+b;
+    console.log("This part will not print");
+}
+
+
+output=sum(3,5)
+console.log(output);
